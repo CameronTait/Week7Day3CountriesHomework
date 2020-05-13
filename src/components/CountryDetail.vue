6fb8b7bf618ca0292903363d817bd5e3d1dd40ad
CameronTait <cameron-tait@hotmail.com>
2020-05-13 22:27:55 +0100

    MVP

---
+++ src/components/CountryDetail.vue
@@ -0,0 +1,21 @@
+<template lang="html">
+  <div v-if='country'>
+    <h3>{{country.name}}</h3>
+    <p>Name: {{country.name}}</p>
+    <p>Capital City: {{country.capital}}</p>
+    <p>Population: {{country.population}}</p>
+  </div>
+</template>
+
+<script>
+import { eventBus } from '../main.js';
+
+export default {
+  name: 'country-detail',
+  props: ['country']
+}
+
+</script>
+
+<style lang="css" scoped>
+</style>
